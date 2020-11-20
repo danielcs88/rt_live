3c891dcb8c0bd360afdaed19f218594d4dc9ae1b
Daniel Cárdenas <danielcs88@users.noreply.github.com>
2020-11-20 01:27:26 -0500

    First run

---
+++ README.md
@@ -0,0 +1,7 @@
+# rt_live
+
+Choropleth map using daily values from [rt.live](https://rt.live/).
+
+## Requirements
+
+- Plotly
---
+++ rt_live.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import ipywidgets as widgets
+import plotly.express as px
+import numpy as np
+
+df = pd.read_csv("https://d14wlfuexuxgcm.cloudfront.net/covid/rt.csv")
